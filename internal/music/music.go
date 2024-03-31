030541fc98c9649fb85c3f887d19b99d1d633bcb
michaelplatt07 <michael@lugia.lugia>
2024-03-31 14:22:41 -0600

    Formatting and clean up of separation of code TODO

--- internal/music/music.go
+++ internal/music/music.go
@@ -3,7 +3,7 @@ package music
 import (
 	"log"
 	"os"
-    "time"
+	"time"
 
 	// MP3 imports
 	"github.com/hajimehoshi/go-mp3"
@@ -15,53 +15,52 @@ import (
 
 // Represents a song and its current state and player
 type Song struct {
-    Name string
-    IsPaused bool
-    Player oto.Player
-    Tag *id3v2.Tag
+	Name     string
+	IsPaused bool
+	Player   oto.Player
+	Tag      *id3v2.Tag
 }
 
 func (song *Song) Title() string {
-    return song.Tag.Title()
+	return song.Tag.Title()
 }
 
 func (song *Song) Artist() string {
-    return song.Tag.Artist()
+	return song.Tag.Artist()
 }
 
 func (song *Song) Album() string {
-    return song.Tag.Album()
+	return song.Tag.Album()
 }
 
 func (song *Song) Genre() string {
-    return song.Tag.Genre()
+	return song.Tag.Genre()
 }
 
 func (song *Song) SaveDetails(title string, artist string, album string, genre string) {
-    song.Tag.SetTitle(title)
-    song.Tag.SetArtist(artist)
-    song.Tag.SetAlbum(album)
-    song.Tag.SetGenre(genre)
+	song.Tag.SetTitle(title)
+	song.Tag.SetArtist(artist)
+	song.Tag.SetAlbum(album)
+	song.Tag.SetGenre(genre)
 
-    if err := song.Tag.Save(); err != nil {
-        log.Fatal("Error while saving a tag: ", err)
-    }
+	if err := song.Tag.Save(); err != nil {
+		log.Fatal("Error while saving a tag: ", err)
+	}
 }
 
 // TODO(map) Figure out wtf to do with this libraryPath param
 func (song *Song) Play(libraryPath string) {
-    // Open the file that is associated with the currently selected song in the queue
-    f, err := os.Open(libraryPath + "/" + song.Name)
-
+	// Open the file that is associated with the currently selected song in the queue
+	f, err := os.Open(libraryPath + "/" + song.Name)
 	if err != nil {
-        log.Println("Err opening file")
+		log.Println("Err opening file")
 		panic(err)
 	}
 	defer f.Close()
 
 	d, err := mp3.NewDecoder(f)
 	if err != nil {
-        log.Println("Err setting up decoder")
+		log.Println("Err setting up decoder")
 		panic(err)
 	}
 
@@ -73,69 +72,71 @@ func (song *Song) Play(libraryPath string) {
 
 	p := c.NewPlayer(d)
 	defer p.Close()
-    log.Println("Playing song")
+	log.Println("Playing song")
 	p.Play()
 
-    // Assign the player so the controls works
-    song.Player = p
+	// Assign the player so the controls works
+	song.Player = p
 
-    // TODO(map) Apply volume adjust
-    p.SetVolume(0.2)
+	// TODO(map) Apply volume adjust
+	p.SetVolume(0.2)
 
-    for {
+	for {
 		time.Sleep(time.Second)
 		if !p.IsPlaying() && !song.IsPaused {
 			break
 		}
 	}
-
 }
 
 // Queue for a list of songs and an index to track
 type Queue struct {
-    Songs []Song
-    CurrIdx int
+	Songs   []Song
+	CurrIdx int
 }
 
 // Library that will load up all the songs available to be played. Differs from Queue in that a Queue is a user defined
 // list of songs to play but Library is everything
 type Library struct {
-    Songs []Song
-    CurrIdx int
-    CurrentSong Song
+	Songs       []Song
+	CurrIdx     int
+	CurrentSong Song
 }
 
 func (library *Library) LoadLibrary(pathToLibrary string) {
-    songs, err := os.ReadDir(pathToLibrary)
-    if err != nil {
-        panic(err)
-    }
-    
-   library.Songs = []Song{}
-   library.CurrIdx = 0
+	songs, err := os.ReadDir(pathToLibrary)
+	if err != nil {
+		panic(err)
+	}
 
-    for _, song := range songs {
-        // TODO(map) song.Name() is actually just returning the file name. It makese sense because of how my stuff is named but really we should be reading the ID3 tags here.
-        log.Printf("Adding song %s to queue", song.Name())
-        // TODO(map) Error handling
-        details, _ := id3v2.Open(pathToLibrary + "/" + song.Name(), id3v2.Options{Parse: true})
-        library.Songs = append(library.Songs, Song{Name: song.Name(), Tag: details, IsPaused: false})
-    }
-    
-    log.Print("Finished loading library...")
+	library.Songs = []Song{}
+	library.CurrIdx = 0
 
-    // Set the current song as the first on
-    library.CurrentSong = library.Songs[library.CurrIdx]
+	for _, song := range songs {
+		// TODO(map) song.Name() is actually just returning the file name. It makese sense because of how my stuff is named but really we should be reading the ID3 tags here.
+		log.Printf("Adding song %s to queue", song.Name())
+		// TODO(map) Error handling
+		details, _ := id3v2.Open(pathToLibrary+"/"+song.Name(), id3v2.Options{Parse: true})
+		library.Songs = append(
+			library.Songs,
+			Song{Name: song.Name(), Tag: details, IsPaused: false},
+		)
+	}
+
+	log.Print("Finished loading library...")
+
+	// Set the current song as the first on
+	library.CurrentSong = library.Songs[library.CurrIdx]
 }
 
 func (library *Library) GetCurrentSong() Song {
-    return library.Songs[library.CurrIdx]
+	return library.Songs[library.CurrIdx]
 }
 
 func (library *Library) IncIndex() {
-    library.CurrIdx += 1
+	library.CurrIdx += 1
 }
 
 func (library *Library) DecIndex() {
-    library.CurrIdx -= 1
+	library.CurrIdx -= 1
 }
--- main.go
+++ main.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	// "fmt"
-	// "log"
-
 	// Internal imports
 	"pomogoro/internal/gui"
 	"pomogoro/internal/music"
@@ -19,7 +16,6 @@ import (
 )
 
 // TODO(map) List of things to correct
-// * Move everything to separate modules to make code nicer and better
 // * Ship with the settings stored within the application itself? This would make them not persistant though
 // * Figure out a nice way to introduce playing music
 // * Fill circle based on percentage of time ran
@@ -89,92 +85,6 @@ func main() {
 	// Library View
 	libraryView := gui.NewLibraryView(libraryListLabelText, &library, songDetailsView)
 
-	// Music controls
-	// prevButton := widget.NewButton(prevButtonText, func() {
-	//     log.Println("Prev clicked")
-	//     // TODO(map) This is totally not safe since it can go out of bounds. Temp measure
-	//     library.DecIndex()
-	//     libraryView.UpdateSelected()
-	//     // TODO(map) This shouldn't start autoplaying if it wasn't playing before hand
-	//     // A few conditions to consider:
-	//     // If music is playing and button clicked, start playing after updating current song
-	//     // If music is paused and button clicked, don't start playing
-	//     // If music is stopped and button clicked, don't start playing
-	//     // if currentSong.Player != nil { // Close the current player
-	//     //     currentSong.Player.Close()
-	//     //     currentSong = library.GetCurrentSong()
-	//
-	//         // Start the song because the previous song was playing
-	//         // go currentSong.Play(settings.LibraryPath)
-	//     // } else {
-	//     //     currentSong = library.GetCurrentSong()
-	//     // }
-	//
-	//     // Set the title to be displayed
-	//     // currentSongPlaying.SetText(fmt.Sprintf("Currently Playing: %s by %s", currentSong.Title(), currentSong.Artist()))
-	//     // currentSongPlaying.Refresh()
-	// })
-	// playButton := widget.NewButton(playButtonText, func() {
-	//     log.Println("Play clicked")
-	//     // if currentSong.Player == nil { // There is no Player set because the initial launch of the MP3 hasn't happened
-	//     //     log.Println("No song set, playing song...")
-	//     //     // libraryList.Select(library.CurrIdx)
-	//     //     // libraryList.Refresh()
-	//     //     go currentSong.Play(settings.LibraryPath)
-	//     //
-	//     //     // Set the title to be displayed
-	//     //     currentSongPlaying.SetText(fmt.Sprintf("Currently Playing: %s by %s", currentSong.Title(), currentSong.Artist()))
-	//     //     currentSongPlaying.Refresh()
-	//     //
-	//     // } else if currentSong.Player.IsPlaying() {
-	//     //     log.Println("Pausing song...")
-	//     //     currentSong.Player.Pause()
-	//     //     currentSong.IsPaused = true
-	//     // } else if !currentSong.Player.IsPlaying() && currentSong.IsPaused {
-	//     //     log.Println("Resuming song...")
-	//     //     currentSong.Player.Play()
-	//     //     currentSong.IsPaused = false
-	//     // }
-	// })
-	// stopButton := widget.NewButton(stopButtonText, func() {
-	//     log.Println("Stop clicked")
-	//     // currentSong.Player.Close()
-	//     // currentSong = library.GetCurrentSong()
-	//     // currentSongPlaying.SetText("Currently Playing: ")
-	// })
-	// nextButton := widget.NewButton(nextButtonText, func() {
-	//     log.Println("Next clicked")
-	//     // TODO(map) This is totally not safe since it can go out of bounds. Temp measure
-	//     library.IncIndex()
-	//     libraryView.UpdateSelected()
-	//     // TODO(map) This shouldn't start autoplaying if it wasn't playing before hand
-	//     // A few conditions to consider:
-	//     // If music is playing and button clicked, start playing after updating current song
-	//     // If music is paused and button clicked, don't start playing
-	//     // If music is stopped and button clicked, don't start playing
-	//     // if currentSong.Player != nil { // Close the current player
-	//     //     currentSong.Player.Close()
-	//     //     currentSong = library.GetCurrentSong()
-	//     //
-	//     //     // Start the song because the previous song was playing
-	//     //     go currentSong.Play(settings.LibraryPath)
-	//     // } else {
-	//     //     currentSong = library.GetCurrentSong()
-	//     // }
-	//
-	//
-	//     // Set the title to be displayed
-	//     // currentSongPlaying.SetText(fmt.Sprintf("Currently Playing: %s by %s", currentSong.Title(), currentSong.Artist()))
-	//     // currentSongPlaying.Refresh()
-	// })
-
-	// Containers around the buttons to ensure their size doesn't grow beyond what is desired
-	// prevButtonContainer := container.New(layout.NewGridWrapLayout(fyne.NewSize(50, 50)), prevButton)
-	// playButtonContainer := container.New(layout.NewGridWrapLayout(fyne.NewSize(50, 50)), playButton)
-	// stopButtonContainer := container.New(layout.NewGridWrapLayout(fyne.NewSize(50, 50)), stopButton)
-	// nextButtonContainer := container.New(layout.NewGridWrapLayout(fyne.NewSize(50, 50)), nextButton)
-
-	// Rows
 	// Info
 	descriptionRow := container.New(
 		layout.NewHBoxLayout(),
@@ -182,8 +92,6 @@ func main() {
 		currentSongPlayingContainer,
 	)
 	// Control
-	// controlsRow := container.New(layout.NewHBoxLayout(), prevButtonContainer, playButtonContainer, stopButtonContainer, nextButtonContainer)
-	// controlsRowParent := container.New(layout.NewCenterLayout(), controlsRow)
 	controls := gui.NewMusicControls(&library, libraryView, settings.LibraryPath)
 
 	// Parent container
